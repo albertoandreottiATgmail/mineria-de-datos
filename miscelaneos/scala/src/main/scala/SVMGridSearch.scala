66eb0087ca1df7f2c05018a27d8c9b05f43cd5e1
Alberto <albertoandreotti@gmail.com>
2019-01-13 23:16:06 -0300

    wip - oversampling minority class

--- miscelaneos/scala/src/main/scala/SVMGridSearch.scala
+++ miscelaneos/scala/src/main/scala/SVMGridSearch.scala
@@ -2,10 +2,13 @@ package unsl.alberto
 import weka.classifiers.Classifier
 import weka.classifiers.Evaluation
 import weka.classifiers.functions.LibSVM
-import weka.core.Instances
+import weka.core.{Instance, Instances}
 import java.io.BufferedReader
 import java.io.FileReader
 import java.util.Random
+
+import weka.filters.Filter
+import weka.filters.supervised.instance.Resample
 
 
 object SVMGridSearch {
@@ -26,12 +29,39 @@ object SVMGridSearch {
     val randData = new Instances(reader)
     reader.close()
 
+
     randData.setClassIndex(randData.numAttributes - 1)
     randData.stratify(folds)
+
+    val resample = new Resample()
+    resample.setBiasToUniformClass(1.0)
+    resample.setInputFormat(randData)
+
+    val filtered = Filter.useFilter(randData, resample).enumerateInstances()
+
+    var cnt = 0
+    var refimprove = 0
+    while(filtered.hasMoreElements) {
+      cnt+=1
+      val e = filtered.nextElement().asInstanceOf[Instance]
+      val v = e.value(randData.numAttributes - 1)
+      if (v == 0.0)
+        refimprove += 1
+
+
+      println(v)
+    }
+
+    println(randData.attributeStats(randData.numAttributes - 1))
+
+    val att = randData.attribute(randData.numAttributes - 1)
+    val refImpIdx = att.indexOfValue("Refimprove")
 
     val outcomes = getRBFCandidates.par.map { cand =>
       val eTestb = new Evaluation(randData)
       val c1Model: Classifier = new LibSVM()
+
+
       c1Model.setOptions(weka.core.Utils.splitOptions(cand.params))
 
       // using seed = 1
@@ -40,7 +70,11 @@ object SVMGridSearch {
 
       println(cand)
       println(eTestb.toClassDetailsString)
+      (eTestb, cand.cc)
     }
+
+    // pick the best one
+    print("best C: " + outcomes.maxBy(_._1.fMeasure(refImpIdx))._2)
   }
 
   def getRBFCandidates = {
