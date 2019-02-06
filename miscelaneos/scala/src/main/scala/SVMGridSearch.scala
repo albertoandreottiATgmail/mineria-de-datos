e696fb6861225c5436cb8b8670f398d3de62fcbd
Alberto <albertoandreotti@gmail.com>
2019-02-05 21:09:08 -0300

    d

--- miscelaneos/scala/src/main/scala/SVMGridSearch.scala
+++ miscelaneos/scala/src/main/scala/SVMGridSearch.scala
@@ -34,52 +34,50 @@ object SVMGridSearch {
     randData.stratify(folds)
 
     val resample = new Resample()
+
     resample.setBiasToUniformClass(1.0)
+    resample.setSampleSizePercent(2.0 / 13.0  * 100)
     resample.setInputFormat(randData)
 
-    val filtered = Filter.useFilter(randData, resample).enumerateInstances()
+    val subsampled = Filter.useFilter(randData, resample)
+    subsampled.stratify(folds)
 
-    var cnt = 0
-    var refimprove = 0
-    while(filtered.hasMoreElements) {
-      cnt+=1
-      val e = filtered.nextElement().asInstanceOf[Instance]
-      val v = e.value(randData.numAttributes - 1)
-      if (v == 0.0)
-        refimprove += 1
-
-
-      println(v)
-    }
-
-    println(randData.attributeStats(randData.numAttributes - 1))
+    val dataset = subsampled
 
     val att = randData.attribute(randData.numAttributes - 1)
     val refImpIdx = att.indexOfValue("Refimprove")
 
-    val outcomes = getRBFCandidates.par.map { cand =>
-      val eTestb = new Evaluation(randData)
+    //val outcomes = getRBFCandidates(3, 15, 2).par.map { cand =>
+    val outcomes = getLinearCandidates(3, 15, 2).par.map { cand =>
+      val eTestb = new Evaluation(dataset)
       val c1Model: Classifier = new LibSVM()
-
 
       c1Model.setOptions(weka.core.Utils.splitOptions(cand.params))
 
       // using seed = 1
       val rand = new Random(1)
-      eTestb.crossValidateModel(c1Model, randData, folds, rand)
+      eTestb.crossValidateModel(c1Model, dataset, folds, rand)
 
-      println(cand)
       println(eTestb.toClassDetailsString)
       (eTestb, cand.cc)
     }
 
+    outcomes.foreach{ case (etest, cc) =>
+      println(cc, etest.fMeasure(refImpIdx))
+    }
     // pick the best one
-    print("best C: " + outcomes.maxBy(_._1.fMeasure(refImpIdx))._2)
+    println("best C: " + outcomes.maxBy(_._1.fMeasure(refImpIdx))._2)
+    println("f score: " + outcomes.maxBy(_._1.fMeasure(refImpIdx))._1.fMeasure(refImpIdx))
   }
 
-  def getRBFCandidates = {
-    for (cg <- Range(-3, 3, 2);cc <- Range(5, 11, 2))
+  def getRBFCandidates(start:Int, end:Int, step:Int) = {
+    for (cg <- Range(-1, 3 + 1, 2);cc <- Range(start, end + 1, step))
         yield RBFParamSet(cg, cc)
+  }
+
+  def getLinearCandidates(start:Int, end:Int, step:Int) = {
+    for (cc <- Range(start, end + 1, step))
+      yield LinearParamSet(cc)
   }
 
 }
@@ -88,4 +86,9 @@ case class RBFParamSet(cg:Int, cc:Int) {
   override def toString: String = {s"cg: 2^$cg, cc:2^$cc"}
   val params = s"-S 0 -K 2 -D 3 -G ${Math.pow(2, cg)} " +
     s"-R 0.0 -N 0.5 -M 40.0 -C ${Math.pow(2, cc)} -E 0.0010 -P 0.1 -Z"
+}
+
+case class LinearParamSet(cc:Int) {
+  override def toString: String = {s"cc:2^$cc"}
+  val params = s"-S 0 -K 0 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C ${Math.pow(2, cc)} -E 0.0010 -P 0.1 -Z"
 }
